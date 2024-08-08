d0734390d267712681576255c75da9f23b08b93b
RonjaPieroth <ronja.pieroth@gmail.com>
2024-08-08 10:00:00 +0200

    removed forecast from html and injected forecast via Js

--- Src/index.js
+++ Src/index.js
@@ -122,7 +122,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<li class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+              <strong>14°C</strong>
+              </div>
+              <div class="weather-forecast-temperature">18°C</div>
+            </div>
+          </li>
+`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 searchCity("Berlin");
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
+
+displayForecast();
